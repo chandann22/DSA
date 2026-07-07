cad8257010f2452c17c696d3ccaf324e8666661c
Chandan Singh <chandansinghatwork22@gmail.com>
2026-07-07 13:28:56 +0530

    Traversal

--- 03-Arrays/largest-in-array.cpp
+++ 03-Arrays/largest-in-array.cpp
@@ -1,1 +1,17 @@
+// Problem: GFG: largest Element in an Array
+// Topic: Arrays
+// Pattern: Traversal
+// Difficulty: Easy
 
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int largest=0;
+        for(int i=0; i<=arr.size()-1; i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        return largest;
+    }
+};
