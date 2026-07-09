01a4d8e9b6b4687d003e530d967864ec90657e29
Chandan Singh <chandansinghatwork22@gmail.com>
2026-07-09 20:08:31 +0530

    Traversal

--- 03-Arrays/check-if-Array-is-Sorted-and-Rotated.cpp
+++ 03-Arrays/check-if-Array-is-Sorted-and-Rotated.cpp
@@ -1,1 +1,17 @@
+// Problem: Leetcode 1752: Check if Array Is Sorted and Rotated
+// Topic: Arrays
+// Pattern: Traversal
+// Difficulty: Easy
 
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int dec=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>nums[(i+1)%nums.size()]){
+                dec++;
+            }
+        }
+        return dec <= 1;
+    }
+};
