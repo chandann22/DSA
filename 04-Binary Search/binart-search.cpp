463c9135dfad66764e02d118f7b4f9ca09e22baf
Chandan Singh <chandansinghatwork22@gmail.com>
2026-07-23 21:02:28 +0530

    Binary Search

---
+++ 04-Binary Search/binart-search.cpp
@@ -0,0 +1,26 @@
+// Problem: Leetcode 704: Binary Search
+// Topic: Arrays
+// Pattern: Binary Search
+// Difficulty: Easy
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        while(left<=right){
+            int mid = (left + right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
