e84b713486dc1a1f75f8bcae80b48a67de26fc3a
Chandan Singh <chandansinghatwork22@gmail.com>
2026-07-26 12:14:49 +0530

    Matrix (Transpose + Reverse)

---
+++ 03-Arrays/rotate-image.cpp
@@ -0,0 +1,18 @@
+// Problem: Leetcode 48: Rotate Image
+// Topic: Arrays
+// Pattern: Matrix (Transpose + Reverse)
+// Difficulty: Medium
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        vector<vector<int>> ans(n, vector<int>(n));
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                ans[j][n-1-i]=matrix[i][j];
+            }
+        }
+        matrix = ans;
+    }
+};
