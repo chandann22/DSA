098f17447ed060bb68e59bca12d25df6bc56c236
Chandan Singh <chandansinghatwork22@gmail.com>
2026-07-31 20:03:01 +0530

    Intervals

---
+++ 03-Arrays/merge-intervals.cpp
@@ -0,0 +1,22 @@
+// Problem: Leetcode 56: Merge Intervals
+// Topic: Arrays
+// Pattern: Intervals
+// Difficulty: Medium
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[0]);
+        for(int i = 1; i<intervals.size(); i++){
+            if(intervals[i][0]<=ans[ans.size()-1][1]){
+                ans[ans.size()-1][1] = max(ans[ans.size()-1][1], intervals[i][1]);
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
