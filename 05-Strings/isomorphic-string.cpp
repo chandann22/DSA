5a86dfde6f93e5eb1a88f5d793df16cb03cec3b6
Chandan Singh <chandansinghatwork22@gmail.com>
2026-08-04 16:26:27 +0530

    Hashing

---
+++ 05-Strings/isomorphic-string.cpp
@@ -0,0 +1,31 @@
+// Problem: Leetcode 205: Isomorphic String
+// Topic: Strings
+// Pattern: Hashing
+// Difficulty: Easy
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> map1;
+        unordered_map<char, char> map2;
+        for(int i = 0; i<s.size(); i++){
+            if(map1.count(s[i])){
+                if(map1[s[i]] != t[i]){
+                    return false;
+                }
+            }
+            else{
+                    map1[s[i]] = t[i];
+                }
+            if(map2.count(t[i])){
+                if(map2[t[i]] != s[i]){
+                    return false;
+                }
+            }
+            else{
+                    map2[t[i]] = s[i];
+                }
+        }
+        return true;
+    }
+};
