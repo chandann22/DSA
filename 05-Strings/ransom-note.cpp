072a709cc4727d1eb2514b55ca06538f77ec915e
Chandan Singh <chandansinghatwork22@gmail.com>
2026-08-05 21:30:52 +0530

    Hashing

---
+++ 05-Strings/ransom-note.cpp
@@ -0,0 +1,23 @@
+// Problem: Leetcode 383: Ransom Note
+// Topic: Strings
+// Pattern: Hashing
+// Difficulty: Easy
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26, 0);
+        for(int i = 0; i<magazine.size(); i++){
+            freq[magazine[i]-'a']++;
+        }
+        for(int i = 0; i<ransomNote.size(); i++){
+            freq[ransomNote[i]-'a']--;
+        }
+        for(int i = 0; i<26; i++){
+            if(freq[i] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
