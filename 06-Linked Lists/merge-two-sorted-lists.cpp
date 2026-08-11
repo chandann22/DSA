ca0680fde9de73b5aee9b390443d074465a69d4c
Chandan Singh <chandansinghatwork22@gmail.com>
2026-08-11 22:43:07 +0530

    Two Pointers

---
+++ 06-Linked Lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,42 @@
+// Problem: Leetcode 21: Merge Two Sorted Lists
+// Topic: Linked List
+// Pattern: Two Pointers
+// Difficulty: Easy
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        vector<int> arr;
+        while(temp1 != NULL){
+            arr.push_back(temp1 -> val);
+                temp1 = temp1 -> next;
+            }
+            while(temp2 != NULL){
+            arr.push_back(temp2 -> val);
+                temp2 = temp2 -> next;
+            }
+        sort(arr.begin(), arr.end());
+        if(arr.empty()){
+            return NULL;
+        }
+        ListNode* head = new ListNode(arr[0]);
+        ListNode* temp = head;
+        for(int i = 1; i < arr.size(); i++){
+            temp -> next = new ListNode(arr[i]);
+            temp = temp -> next;
+        }
+        return head;
+    }
+};
